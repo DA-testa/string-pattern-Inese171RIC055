e5c54c5d7c17bf490c242328c1ff99a2085ece4a
Inese1k5g <125188976+Inese171RIC055@users.noreply.github.com>
2023-04-07 12:22:58 +0000

    versija 1

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,51 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    # acquire input from keyboard or file
+    input_type = input().rstrip().lower()
+    if input_type == "i":  # input from keyboard
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == "f":  # input from file
+        with open("tests/06", "r") as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+    else:
+        raise ValueError("Invalid input type choice")
+    return (pattern, text)
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+    # control output
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    # find occurrences using Rabin-Karp algorithm
+    n = len(text)
+    m = len(pattern)
+    p = 31  # chosen prime number
+    d = 256  # number of possible characters
+    
+    # Calculate hash value of pattern and first substring of text
+    pattern_hash = 0
+    substring_hash = 0
+    h = 1
+    for i in range(m-1):
+        h = (h * d) % p
+    for i in range(m):
+        pattern_hash = (pattern_hash * d + ord(pattern[i])) % p
+        substring_hash = (substring_hash * d + ord(text[i])) % p
+    
+    # Check for match in substrings
+    occurrences = []
+    for i in range(n-m+1):
+        if pattern_hash == substring_hash:
+            if text[i:i+m] == pattern:
+                occurrences.append(i)
+        if i < n-m:
+            substring_hash = (d * (substring_hash - ord(text[i]) * h) + ord(text[i+m])) % p
+    
+    return occurrences
 
-    # and return an iterable variable
-    return [0]
-
-
-# this part launches the functions
+# launch functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
